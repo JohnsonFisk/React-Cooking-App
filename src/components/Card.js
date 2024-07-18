49730bcc566d5eb6a25a11cd3786661af6d2e98d
Josk <131044599+JohnsonFisk@users.noreply.github.com>
2024-07-18 18:58:27 +0200

    FIX : refonte de la barre de navigation (pas responsive encore)

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,7 @@ const Card = ({ recipe }) => {
 
     return (
         <div className="recipe-card">
-            <h2>{recipe.title}</h2>
+            {/* <h2 className='title'>{recipe.title}</h2> */}
             <img src={recipe.image} alt={"Photo" + recipe.title} />
             <div className="info">
                 <h2>{recipe.title}</h2>
