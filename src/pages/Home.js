cfdc143ee4aa75ab07fbcc80d55082ff5d504e6f
Josk <131044599+JohnsonFisk@users.noreply.github.com>
2024-08-06 20:26:50 +0200

    REFONTE DESIGN HOME PAGE

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import Carousel from './Caroussel';
-import Header from './Header';
+import React from "react";
+import Carousel from "./Caroussel";
+import Header from "./Header";
 
 const Home = () => {
     const apiKey = process.env.REACT_APP_SPOONACULAR_API_KEY;
 
     return (
         <div className="container">
-            <Header/> 
+            <Header />
+            <div className="homepage">
+                <header className="hero-section">
+                    <h1>Welcome to Cooking App, your culinary companion!</h1>
+                    <p>Turn every meal into a gourmet feast.</p>
+                    <button>Explore our recipes</button>
+                </header>
+
+                <section className="categories">
+                    <h2>Explore by category</h2>
+                    <div className="category-list">
+                        {/* Add links to categories with images */}
             <Carousel category="entree" apiKey={apiKey} />
             <Carousel category="main course" apiKey={apiKey} />
             <Carousel category="dessert" apiKey={apiKey} />
             <Carousel category="drink" apiKey={apiKey} />
+                    </div>
+                </section>
+            </div>
         </div>
     );
 };
