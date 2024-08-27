6265c865bfc778e4d560220a8994ff5096572b24
Josk <131044599+JohnsonFisk@users.noreply.github.com>
2024-08-27 16:57:32 +0200

    UPTADE plein de truc

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -4,7 +4,17 @@ import { NavLink } from "react-router-dom";
 const Header = () => {
   return (
     <div className="header-container">
-      <h1 className="leftPart"><span><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 14 14"><path fill="none" stroke="#888888" strokeLinecap="round" strokeLinejoin="round" d="M2.5 13.5h9m1.93-10.1a2.49 2.49 0 0 0-4.09-1.26a2.49 2.49 0 0 0-4.68 0A2.49 2.49 0 0 0 .57 3.4A2.51 2.51 0 0 0 2.5 6.45V10a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V6.45a2.51 2.51 0 0 0 1.93-3.05ZM2.5 8.5h9" /></svg> &nbsp;</span>Cooking App</h1>
+      <div className="leftPart">
+        <NavLink to="/" className="logo-link">
+          <span>
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 14 14">
+              <path fill="none" stroke="#888888" strokeLinecap="round" strokeLinejoin="round" d="M2.5 13.5h9m1.93-10.1a2.49 2.49 0 0 0-4.09-1.26a2.49 2.49 0 0 0-4.68 0A2.49 2.49 0 0 0 .57 3.4A2.51 2.51 0 0 0 2.5 6.45V10a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V6.45a2.51 2.51 0 0 0 1.93-3.05ZM2.5 8.5h9" />
+            </svg>
+          </span>
+          <h1>Cooking App</h1>
+        </NavLink>
+      </div>
+
       <nav className="rightPart">
         <div className="nav-container">
           <NavLink
@@ -33,7 +43,11 @@ const Header = () => {
               className="search"
               placeholder="Search for a recipe"
             />
-            <button type="submit" className="search-button"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 256 256"><path fill="currentColor" d="m228.24 219.76l-51.38-51.38a86.15 86.15 0 1 0-8.48 8.48l51.38 51.38a6 6 0 0 0 8.48-8.48M38 112a74 74 0 1 1 74 74a74.09 74.09 0 0 1-74-74" /></svg></button>
+            <button type="submit" className="search-button">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 256 256">
+                <path fill="currentColor" d="m228.24 219.76l-51.38-51.38a86.15 86.15 0 1 0-8.48 8.48l51.38 51.38a6 6 0 0 0 8.48-8.48M38 112a74 74 0 1 1 74 74a74.09 74.09 0 0 1-74-74" />
+              </svg>
+            </button>
           </div>
         </div>
       </nav>
--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import Header from "./Header";
 
 const RecipeDetails = () => {
     const { id } = useParams();
@@ -26,17 +27,49 @@ const RecipeDetails = () => {
     }
 
     return (
+        <div>
+            <Header/>
         <div className="recipe-details">
-            <h1>{recipe.title}</h1>
-            <img src={recipe.image} alt={recipe.title} />
-            <p>{recipe.summary}</p>
-            <ul>
-                {recipe.extendedIngredients.map((ingredient) => (
-                    <li key={ingredient.id}>{ingredient.original}</li>
-                ))}
-            </ul>
-            <h3>Instructions</h3>
-            <div dangerouslySetInnerHTML={{ __html: recipe.instructions }} />
+            <div className="recipe-header">
+                <div className="recipe-image">
+                    <img src={recipe.image} alt={recipe.title} />
+                </div>
+                <div className="recipe-info">
+                    <h1>{recipe.title}</h1>
+                    <ul>
+                        {recipe.extendedIngredients.map((ingredient) => (
+                            <li key={ingredient.id}>{ingredient.original}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+
+            <div className="recipe-instructions">
+                <h3>Instructions</h3>
+                <div dangerouslySetInnerHTML={{ __html: recipe.instructions }} />
+
+                {recipe.analyzedInstructions && recipe.analyzedInstructions.length > 0 && (
+                    <div className="analyzed-instructions">
+                        {recipe.analyzedInstructions[0].steps.map((step) => (
+                            <div key={step.number} className="instruction-step">
+                                <h4>Step {step.number}</h4>
+                                <p>{step.step}</p>
+                                {step.ingredients && step.ingredients.length > 0 && (
+                                    <div className="ingredients-list">
+                                        <strong>Ingredients:</strong>
+                                        <ul>
+                                            {step.ingredients.map((ingredient, index) => (
+                                                <li key={index}>{ingredient.name}</li>
+                                            ))}
+                                        </ul>
+                                    </div>
+                                )}
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
+        </div>
         </div>
     );
 };
