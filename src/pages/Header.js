f148fb74224059646d9cdbd00e2ad54a812d43b5
JohnsonFisk <131044599+JohnsonFisk@users.noreply.github.com>
2024-09-14 01:13:53 +0200

    UPTADE : Responsive header.js

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -23,6 +23,9 @@ const Header = () => {
     <div className="header-container">
       <div className="leftPart">
         <NavLink to="/" className="logo-link">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 14 14">
+          <path fill="none" stroke="#888888" strokeLinecap="round" strokeLinejoin="round" d="M2.5 13.5h9m1.93-10.1a2.49 2.49 0 0 0-4.09-1.26a2.49 2.49 0 0 0-4.68 0A2.49 2.49 0 0 0 .57 3.4A2.51 2.51 0 0 0 2.5 6.45V10a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V6.45a2.51 2.51 0 0 0 1.93-3.05ZM2.5 8.5h9" />
+          </svg>
           <h1>Cooking App</h1>
         </NavLink>
       </div>
@@ -50,7 +53,7 @@ const Header = () => {
               onChange={inputResult}
             />
             <button type="submit" className="search-button" onClick={searchResult}>
-              Rechercher
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#ffffff" d="m19.485 20.154l-6.262-6.262q-.75.639-1.725.989t-1.96.35q-2.402 0-4.066-1.663T3.808 9.503T5.47 5.436t4.064-1.667t4.068 1.664T15.268 9.5q0 1.042-.369 2.017t-.97 1.668l6.262 6.261zM9.539 14.23q1.99 0 3.36-1.37t1.37-3.361t-1.37-3.36t-3.36-1.37t-3.361 1.37t-1.37 3.36t1.37 3.36t3.36 1.37"/></svg>
             </button>
           </div>
         </div>
